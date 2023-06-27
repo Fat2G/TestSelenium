efb7e567bf9f3ff832eabfd592c605991af62960
Fat2G <fatougassa@gmail.com>
2023-06-27 13:29:24 +0200

    tests vérifiés

--- src/test/java/TestDemoqa.java
+++ src/test/java/TestDemoqa.java
@@ -1,7 +1,10 @@
 import org.junit.Ignore;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -17,9 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestDemoqa {
-    //    private WebDriver driver;
     List<WebDriver> drivers = new ArrayList<>();
-
 
     @BeforeEach
     public void setUp() {
@@ -45,12 +46,12 @@ public class TestDemoqa {
             WebElement btnDbleClk = driver.findElement(By.id("doubleClickBtn"));
 
             actions.doubleClick(btnDbleClk).perform();
-            Thread.sleep(500);
+            Thread.sleep(1000);
 
             WebElement dbleClkMsg = driver.findElement(By.id("doubleClickMessage"));
             assertEquals("You have done a double click", dbleClkMsg.getText());
 
-            System.out.println("Test passé avec " + driver.getClass().getSimpleName());
+            System.out.println("Test double click passé avec " + driver.getClass().getSimpleName());
         }
     }
 
@@ -69,70 +70,32 @@ public class TestDemoqa {
             WebElement btnRightMsg = driver.findElement(By.id("rightClickMessage"));
             assertEquals("You have done a right click", btnRightMsg.getText());
 
-            System.out.println("Test passé avec " + driver.getClass().getSimpleName());
+            System.out.println("Test click droit passé avec " + driver.getClass().getSimpleName());
         }
 
     }
 
     @Ignore
-    public void it_should_not_register() throws InterruptedException {
+    public void it_should_select_radio_yes() throws InterruptedException {
         for (WebDriver driver : drivers) {
 
-            driver.get("https://demoqa.com/register");
+            driver.get("https://demoqa.com/radio-button");
 
-            WebElement firstName = driver.findElement(By.id("firstname"));
-            WebElement lastName = driver.findElement(By.id("lastname"));
-            WebElement userName = driver.findElement(By.id("userName"));
-            WebElement password = driver.findElement(By.id("password"));
+//            WebElement yesBtnRadio = driver.findElement(By.id("yesRadio"));
+            WebElement yesBtnRadio = driver.findElement(By.name("like"));
+            yesBtnRadio.click();
 
-            firstName.sendKeys("Fat");
-            lastName.sendKeys("G");
-            userName.sendKeys("FatG");
-            password.sendKeys("Infotel1!");
+            Thread.sleep(1000);
 
-            Thread.sleep(3000);
-
-            WebElement captchaCheckbox = driver.findElement(By.xpath("//span[@class='recaptcha-checkbox-borderAnimation']"));
-            captchaCheckbox.click();
-            Thread.sleep(3000);
-
-            WebElement submitButton = driver.findElement(By.id("register"));
-            submitButton.click();
-
-            Thread.sleep(500);
-            WebElement userExists = driver.findElement(By.id("name"));
-            assertEquals("User exists!", userExists.getText());
-
-        }
-    }
-
-    @Test
-    public void it_should_login() throws InterruptedException {
-
-        for (WebDriver driver : drivers) {
-
-            driver.get("https://demoqa.com/login");
-
-            WebElement userName = driver.findElement(By.id("userName"));
-            WebElement password = driver.findElement(By.id("password"));
-
-            userName.sendKeys("FatG");
-            password.sendKeys("Infotel1!");
-
-            WebElement submitButton = driver.findElement(By.id("login"));
-            submitButton.click();
-            Thread.sleep(500);
-
-            // vérification que la page profile soit bien ouverte
-            WebElement profileHeader = driver.findElement(By.className("main-header"));
-            assertEquals("Profile", profileHeader.getText());
+            // vérification du message de succes
+            WebElement msgSuccess = driver.findElement(By.className("text-success"));
+            assertEquals("Yes", msgSuccess.getText());
 
             driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-            // vérification de l'url de la page de profil
-            String currentUrl = driver.getCurrentUrl();
-            assertTrue(currentUrl.contains("profile"));
-            System.out.println("Test passé avec " + driver.getClass().getSimpleName());
+
+            System.out.println("Test radio \"Yes\" passé avec " + driver.getClass().getSimpleName());
         }
+
     }
 
     @Test
@@ -182,8 +145,73 @@ public class TestDemoqa {
             }
 
             assertTrue(contentFound);
+            System.out.println("Test web table passé avec " + driver.getClass().getSimpleName());
         }
     }
+
+    @Ignore
+    public void it_should_not_register() throws InterruptedException {
+        for (WebDriver driver : drivers) {
+
+            driver.get("https://demoqa.com/register");
+
+            WebElement firstName = driver.findElement(By.id("firstname"));
+            WebElement lastName = driver.findElement(By.id("lastname"));
+            WebElement userName = driver.findElement(By.id("userName"));
+            WebElement password = driver.findElement(By.id("password"));
+
+            firstName.sendKeys("Fat");
+            lastName.sendKeys("G");
+            userName.sendKeys("FatG");
+            password.sendKeys("Infotel1!");
+
+            Thread.sleep(3000);
+
+            Actions actions = new Actions(driver);
+            WebElement captchaCheckbox = driver.findElement(By.className("recaptcha-checkbox-border"));
+//            captchaCheckbox.click();
+            actions.click(captchaCheckbox).perform();
+            Thread.sleep(1000);
+
+            WebElement submitButton = driver.findElement(By.id("register"));
+            submitButton.click();
+
+            Thread.sleep(500);
+            WebElement userExists = driver.findElement(By.id("name"));
+            assertEquals("User exists!", userExists.getText());
+
+        }
+    }
+
+    @Test
+    public void it_should_login() throws InterruptedException {
+
+        for (WebDriver driver : drivers) {
+
+            driver.get("https://demoqa.com/login");
+
+            WebElement userName = driver.findElement(By.id("userName"));
+            WebElement password = driver.findElement(By.id("password"));
+
+            userName.sendKeys("FatG");
+            password.sendKeys("Infotel1!");
+
+            WebElement submitButton = driver.findElement(By.id("login"));
+            submitButton.click();
+            Thread.sleep(500);
+
+            // vérification que la page profile soit bien ouverte
+            WebElement profileHeader = driver.findElement(By.className("main-header"));
+            assertEquals("Profile", profileHeader.getText());
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+            // vérification de l'url de la page de profil
+            String currentUrl = driver.getCurrentUrl();
+            assertTrue(currentUrl.contains("profile"));
+            System.out.println("Test login passé avec " + driver.getClass().getSimpleName());
+        }
+    }
+
 }
 
 
