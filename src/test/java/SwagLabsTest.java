03c488d8f336537819f10fefd57f6cea44326954
Fat2G <fatougassa@gmail.com>
2023-06-29 15:50:11 +0200

    it_should_open_item_detail_by_img() test OK

--- src/test/java/SwagLabsTest.java
+++ src/test/java/SwagLabsTest.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(Enclosed.class)
 public class SwagLabsTest {
@@ -33,6 +35,7 @@ public class SwagLabsTest {
         @AfterEach
         public void quit() {
             driver.quit();
+            driver.close();
         }
 
         public void printMsg(String name) {
@@ -70,5 +73,66 @@ public class SwagLabsTest {
                 printMsg("login");
 
         }
+
+        //TODO: externaliser dans une classe
+        @Test
+        public void it_should_not_login() throws InterruptedException {
+
+            driver.get("https://www.saucedemo.com/");
+
+            driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+            driver.findElement(By.id("password")).sendKeys(this.password);
+
+            driver.findElement(By.id("login-button")).click();
+            Thread.sleep(1000);
+
+            String errorMsg = "Epic sadface: Sorry, this user has been locked out.";
+
+            assertFalse(driver.getCurrentUrl().contains("inventory.html"));
+            assertEquals(errorMsg, driver.findElement(By.cssSelector("div.error-message-container.error>h3")).getText());
+
+            printMsg("error login");
+
+        }
     }
+
+    @RunWith(Parameterized.class)
+    public static class Item {
+        private WebDriver driver;
+
+        public Item(WebDriver driver) {
+            this.driver = driver;
+        }
+
+        public void connect(){
+            driver.findElement(By.id("user-name")).sendKeys("standard_user");
+            driver.findElement(By.id("password")).sendKeys("secret_sauce");
+            driver.findElement(By.id("login-button")).click();
+        }
+
+        @Parameterized.Parameters
+        public static List<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+                    {new ChromeDriver()},
+                    {new FirefoxDriver()}
+            });
+        }
+
+        @Test
+        public void it_should_open_item_detail_by_img() throws InterruptedException {
+            driver.get("https://www.saucedemo.com/");
+            connect();
+
+            WebElement itemLink = driver.findElement(By.id("item_4_img_link"));
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            wait.until(ExpectedConditions.elementToBeClickable(itemLink));
+
+            itemLink.click();
+            Thread.sleep(1000);
+
+            assertTrue(driver.getCurrentUrl().contains("?id=4"));
+        }
+    }
+
 }
