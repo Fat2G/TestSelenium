79b2446e0bac1ba4d9d6a88e2b4235944d1011b8
Fat2G <fatougassa@gmail.com>
2023-07-05 10:13:52 +0200

    cucumber report implemented

--- src/test/java/cucumber/CucumberIT.java
+++ src/test/java/cucumber/CucumberIT.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"pretty", "html:target/report/cucumber.html",
+                "json:target/report/cucumber.json"},
         features = "src/test/resources/features",
         glue = "cucumber"
 )
